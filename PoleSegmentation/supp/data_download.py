089680e58ca4a6a61636a4b16a5b1f661e600e82
CMB <56138926+catherine-m-breen@users.noreply.github.com>
2023-03-10 10:04:54 +0100

    update data_download.py to see data structure

--- PoleSegmentation/supp/data_download.py
+++ PoleSegmentation/supp/data_download.py
@@ -2,7 +2,7 @@ import os
 import tarfile
 from torchvision.datasets.utils import download_url, check_integrity
             
-root = '/home/zhongqimiao/ssdprivate/datasets/VOC'
+root = '/Users/catherinebreen/Chapter1/datasets/VOC'
 url = 'http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar'
 filename = 'VOCtrainval_11-May-2012.tar'
 md5 = '6cd6e144f989b92b3379bac3b3de84fd'
